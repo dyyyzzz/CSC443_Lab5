594936f6737469b820b64b1008926db57f204215
dyyyzzz <nuradilah688@gmail.com>
2022-05-17 14:50:29 +0800

    commit

--- indexedArray.php
+++ indexedArray.php
@@ -7,6 +7,8 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
 $month = array ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+?>
 </body>
 </html>
