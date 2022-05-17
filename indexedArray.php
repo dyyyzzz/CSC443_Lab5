6ac41bfb2416c8bbe7ae0152e23a5bcd6d984e98
dyyyzzz <nuradilah688@gmail.com>
2022-05-17 15:21:28 +0800

    commit

--- indexedArray.php
+++ indexedArray.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     <?php
     $month[0] = 'January'; 
     $month[1] = 'February';
@@ -23,6 +15,3 @@
 
     asort($month, SORT_NUMERIC);
     ?>
-
-</body>
-</html>
