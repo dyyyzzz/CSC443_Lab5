daaae064497b90ab97b96c9109a5a8df1ec62bea
dyyyzzz <nuradilah688@gmail.com>
2022-05-17 15:24:32 +0800

    commit

--- indexedArray.php
+++ indexedArray.php
@@ -1,19 +1,17 @@
 
     <?php
-  echo $month[0] = 'January'; 
-  echo $month[1] = 'February';
-  echo  $month[2] = 'March';
-  echo  $month[2] = 'March';
-  echo $month[2] = 'March';
-  echo $month[3] = 'April';
-  echo $month[4] = 'May';
-  echo $month[5] = 'June';
-  echo $month[6] = 'July';
-  echo  $month[7] = 'August';
-  echo  $month[8] = 'September';
-  echo $month[9] = 'October';
-  echo $month[10] = 'November';
-  echo $month[11] = 'December';
+    $month[0] = 'January'; 
+    $month[1] = 'February';
+    $month[2] = 'March';
+    $month[3] = 'April';
+    $month[4] = 'May';
+    $month[5] = 'June';
+    $month[6] = 'July';
+    $month[7] = 'August';
+    $month[8] = 'September';
+    $month[9] = 'October';
+    $month[10] = 'November';
+    $month[11] = 'December';
 
-    asort($month, SORT_NUMERIC);
+    echo asort($month, SORT_NUMERIC);
     ?>
