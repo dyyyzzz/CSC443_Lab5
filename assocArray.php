21840da4c814caad3318c84b97558706af30838c
dyyyzzz <nuradilah688@gmail.com>
2022-05-17 23:40:08 +0800

    commit

--- assocArray.php
+++ assocArray.php
@@ -1,16 +1,32 @@
 <?php
+    $month[0] = 'January'; 
+    $month[1] = 'February';
+    $month[2] = 'March';
+    $month[3] = 'April';
+    $month[4] = 'May';
+    $month[5] = 'June';
+    $month[6] = 'July';
+    $month[7] = 'August';
+    $month[8] = 'September';
+    $month[9] = 'October';
+    $month[10] = 'November';
+    $month[11] = 'December';
+
 $monthDays = array ('January'   => 31, 'February'  => 28,
                      'March'    => 31,  'April'    => 20,
                      'May'      => 31,  'June'     => 30,
                      'July'     => 31,  'August'   => 31,
                      'September'=> 30,  ' October' => 31,
                      'November' => 30,  'December' => 31);
+
+    $i = 0;
+    foreach ($monthDays as $days)
+    {
+        echo "$month[$i] - $days <br>";
+        $i++;
+    }
+    echo "<br>";
 ?>
-<TABLE border="1">
-    <TR>
-        <TD><B>MONTH</B></TD>
-        <TD><B>DAYS</B></TD>
-</TR>
-<?php
+
 
 
